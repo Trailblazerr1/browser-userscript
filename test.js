866c8434d54d8426ea8539e9fcc9afc63af5b99c
Anurag kushwaha <kanurag078@gmail.com>
2021-03-10 21:35:58 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name    SmartCookieWeb userscript @require test
+// @version 1
+// @match   https://smartcookieweb.com/
+// ==/UserScript==
+
+try {
+	alert(SCW);
+}
+catch (error) {
+	alert(error);
+}
