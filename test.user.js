c36ee90e1209efb18707a09e313285a903bebff4
Anurag kushwaha <kanurag078@gmail.com>
2021-03-10 22:24:00 +0530

    Update formatting and url

--- test.user.js
+++ test.user.js
@@ -3,11 +3,18 @@
 // @version 0.1
 // @description Script to make web browsing better on e-ink
 // @author Anurag
-// @match   https://www.google.com/*
+// @match   https://www.*.*/*
 // ==/UserScript==
 
 (function () {
- var htmls = document.getElementsByTagName('*'); var i = htmls.length - 1; var elem; while (i) { elem = htmls[i]; if (elem && elem.style) { elem.style.background = '#fff'; elem.style.color = '#000'; elem.style.wordWrap = "break-word"; elem.style.whiteSpace = "pre-wrap"; if(['PRE', 'CODE'].indexOf(elem.tagName) > -1) { elem.style.whiteSpace = "pre-wrap"; } } i--; }
- })
- ();
+ var htmls = document.getElementsByTagName('*'); 
+ var i = htmls.length - 1; 
+ var elem; 
+ while (i) {
+  elem = htmls[i]; 
+  if (elem && elem.style) {
+   elem.style.background = '#fff'; elem.style.color = '#000'; elem.style.wordWrap = "break-word"; elem.style.whiteSpace = "pre-wrap"; if(['PRE', 'CODE'].indexOf(elem.tagName) > -1) { elem.style.whiteSpace = "pre-wrap"; } 
+   } i--; 
+  }
+ })();
 
