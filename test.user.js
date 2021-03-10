92c3e0c7876695faebe750b00648c1bb7efab1ce
Anurag kushwaha <kanurag078@gmail.com>
2021-03-10 21:48:53 +0530

    Update script for e-ink test

--- test.user.js
+++ test.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name    SmartCookieWeb userscript @require test
-// @version 1
-// @match   https://smartcookieweb.com/
+// @name    e-ink browser test script
+// @version 0.1
+// @description Script to make web browsing better on e-ink
+// @author Anurag
+// @match   *
 // ==/UserScript==
 
-try {
-	alert(SCW);
-}
-catch (error) {
-	alert(error);
-}
+(function () {
+ var htmls = document.getElementsByTagName('*'); var i = htmls.length - 1; var elem; while (i) { elem = htmls[i]; if (elem && elem.style) { elem.style.background = '#fff'; elem.style.color = '#000'; elem.style.wordWrap = "break-word"; elem.style.whiteSpace = "pre-wrap"; if(['PRE', 'CODE'].indexOf(elem.tagName) > -1) { elem.style.whiteSpace = "pre-wrap"; } } i--; }
+ })
+ ();
+
